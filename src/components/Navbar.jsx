88210bbcdf7dd44a8cba3fe64d3bbf57c390d58f
user <user@usernoMacBook-Air.local>
2024-09-02 14:15:20 +0900

    toppage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
+import {appleImg, bagImg, searchImg} from '../utils';
+import {navLists} from '../constants';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='w-full py-5 sm:px-10 px-5 flex justify-between item-center'>
+      <nav className='flex w-full screen-max-width '>
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+
+        <dir className='flex flex-1 justify-center max-sm:hidden'>
+          {navLists.map((nav) => (
+            <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
+              {nav}
+            </div>
+
+          ))}
+
+          <div>
+            <img src={searchImg} alt="search" width={18} height={18} />
+            <img src={bagImg} alt="bag" width={18} height={18} />
+          </div>
+        </dir>
+      </nav>
+    </header>
   )
 }
 
