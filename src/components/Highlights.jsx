6373dd2f98929db0f47203f0d95dff5c0ba2e460
user <user@usernoMacBook-Air.local>
2024-09-03 14:12:53 +0900

    img text

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,11 +1,13 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { rightImg, watchImg } from "../utils"
+import VideoCarousel from "./VideoCarousel";
+
 
 const Highlights = () => {
   useGSAP(() => {
     gsap.to('#title',{ opacity: 1,y: 0})
-    gsap.to('.link',{ opacity: 1,y:0 })
+    gsap.to('.link',{ opacity: 1,y: 0,duration: 1,stagger: 0.25})
   },[])
 
   return (
@@ -17,7 +19,6 @@ const Highlights = () => {
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
-        </div>
 
         <div className="flex flex-wrap items-end gap-5">
           <p className="link">
@@ -30,6 +31,10 @@ const Highlights = () => {
             <img src={rightImg} alt="right" className="ml-2" />
           </p>
         </div>
+      </div>
+
+      <VideoCarousel />
+
       </div>
     </section>
   )
---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,97 @@
+
+import { useEffect, useRef, useState} from 'react'
+import {hightlightsSlides} from '../constants'
+
+
+const VideoCarousel = () => {
+
+    const videoRef = useRef([]);
+    const videoSpanRef = useRef([]);
+    const videoDivRef = useRef([]);
+
+    const [video,setVideo] = useState({
+        isEnd: false,
+        startPlay: false,
+        videoId: 0,
+        isLastvideo: false,
+        isPlaying: false, 
+    })
+    const [loadedData,setLoadedData] = useState([]);
+    const {isEnd,startPlay,videoId,isLastvideo,isPlaying} = video;
+
+    useEffect(() => {
+      if(loadedData.length > 3){
+        if(!isPlaying){
+            videoRef.current[videoId].pause();
+        }else{
+            startPlay && videoRef.current[videoId].play();
+        }
+      }
+    }, [startPlay,videoId,isPlaying,loadedData])
+    
+
+    useEffect(() => {
+        const currentProgress = 0;
+        let span = videoSpanRef.current;
+
+        if(span[videoId]){
+            let anim = gsap.to(span[videoId],{
+                onUpdate: () => {
+
+                },
+                onComplete: () => {
+
+                }
+            })
+        }
+    },[])
+
+    return (
+
+        
+        <>
+        <h1>videocarousel</h1>
+
+            <div className='flex items-center'>
+                {hightlightsSlides.map((list,i) => (
+                    <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+                        <div className='video-carousel_container'>
+
+                            {/* video部分------------- */}
+                            <div className='w-full h-full 
+                            flex-center rounded-3xl overflow-hidden bg-black'>
+                                <video id="video" 
+                                    playsInline={true} 
+                                    preload='auto' 
+                                    muted
+                                // 追加--------------------
+                                    ref = {(el) => (videoRef.current[i] = el)}
+                                    onPlay={() => {
+                                        setVideo((preVideo) => ({
+                                            ...preVideo,isPlaying: true
+                                        }))
+                                    }}
+                                >
+                                    <source src={list.video} type="video/mp4" />
+                                </video>
+                            </div>
+                            
+                            {/* comment部分------------------- */}
+                            <div className="absolute top-12 left-[5%] z-10 ">
+                                {list.textLists.map((text) => (
+                                    <p key={text} className="md:text-2xl text-xl font-medium"  >
+                                        {text}
+                                    </p>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className=" "></div>
+        </>
+    )
+}
+
+export default VideoCarousel
